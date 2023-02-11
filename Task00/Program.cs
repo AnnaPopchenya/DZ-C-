47b7319f437664b83fc52a58f8cb2a6f2b530aa9
Anna <popchenya995@mail.ru>
2023-02-11 11:42:50 +0300

    исправила задачу Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,25 +1,15 @@
-﻿// 1. Демонстрация решения
+﻿// 0. Демонстрация решения
 
 // Напишите программу,
-// 1. которая на вход принимает два числа и
-// 2. проверяет, является ли первое число квадратом второго.
+// 1. которая на вход принимает число и
+// 2. выдаёт его квадрат (число умноженное на само себя).
 
 // Например:
-// a = 25, b = 5 -> да
-// a = 2, b = 10 -> нет
-// a = 9, b = -3 -> да
-// a = -3, b = 9 -> нет 
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
 
-Console.WriteLine("Введите число первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число первое число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if(number1 / number2 == number2) 
-{
-    Console.WriteLine("первое число является квадратом второго");
-}
-else
-{
-Console.WriteLine("первое число не является квадратом второго");
-}
+Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine ($"Квадрат числа {number} = {square}");
--- Task05/Program.cs
+++ Task05/Program.cs
@@ -8,7 +8,7 @@
 Console.WriteLine("Введите натуральное число");
 int number = Convert.ToInt32(Console.ReadLine());
 int index = -number;
-while (index <= number);
+while (index <= number)
 {
     Console.Write($"{index}");
     index++;

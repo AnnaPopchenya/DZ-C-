71d685017b647017da8dd44356fbdff676e37da8
Anna <popchenya995@mail.ru>
2023-02-11 20:43:58 +0300

    Внесла исправления в Task04 и Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -8,11 +8,11 @@
 Console.WriteLine("Введите натуральное число");
 int number = Convert.ToInt32(Console.ReadLine());
 int index = 1;
-while (index <= number) 
+while (index <= number)
 {
-if (index % 2 == 0) 
-{
-    Console.Write($"{index} ");
-}
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index} ");
+    }
     index++;
 }

5a9b7694e71aefe8a607c79c601513073985b8b7
Anna <popchenya995@mail.ru>
2023-02-11 15:23:07 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, 
+// 1.которая на вход принимает число (N), 
+// 2.а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= number) 
+{
+if (index % 2 == 0) 
+{
+    Console.Write($"{index} ");
+}
+    index++;
+}

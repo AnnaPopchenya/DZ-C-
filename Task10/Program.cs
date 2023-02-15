3959adb7313331d611e31a8266b36515beb5bd99
Anna <popchenya995@mail.ru>
2023-02-15 22:54:28 +0300

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая 
+// 1.принимает на вход трёхзначное число и 
+// 2.на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+// int result = ( number /10 ) % 10;
+// Console.WriteLine($"Второе число этого числа {result}");
+
+int newNumber = NewNumber (number);
+Console.WriteLine($"Второе число этого числа {newNumber}");
+
+int NewNumber (int num)
+{
+    int result = ( number /10 ) % 10;
+    return result;
+}

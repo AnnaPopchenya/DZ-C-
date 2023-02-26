9ef9da899ea7abb8ae0d7190ef71ed566cc85384
Anna <popchenya995@mail.ru>
2023-02-26 22:23:32 +0300

    Added 35,37,38

---
+++ Task38/Program.cs
@@ -0,0 +1,21 @@
+﻿double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}

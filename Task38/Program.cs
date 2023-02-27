d27e6c97c7aaab2ed663662a8d1a044d6d3e822b
Anna <popchenya995@mail.ru>
2023-02-27 15:37:35 +0300

    Task 34,36,38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,4 +1,9 @@
-﻿double[] CreateArrayRndDouble(int size, int min, int max)
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
@@ -19,3 +24,39 @@ void PrintArrayDouble(double[] arr)
     }
     Console.Write("]");
 }
+
+double MaxElement(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+double MinElement(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+double DifferenceMaxMinElement(double max, double min)
+{
+    double difference = Math.Round((max-min), 1, MidpointRounding.ToZero);
+    return difference;
+}
+
+// Console.WriteLine(Math.Round(differenceMaxMinElement, 1, MidpointRounding.ToZero));
+
+double[] array = CreateArrayRndDouble(4, 1, 50);
+PrintArrayDouble(array);
+double minElement = MinElement(array);
+double maxElement = MaxElement(array);
+double differenceMaxMinElement = MaxElement(array) - MinElement(array);
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным элементом {maxElement} и минимальным {minElement} равна {differenceMaxMinElement}");

de9c5994aad11088015e08c07ddafd1153cef662
Anna <popchenya995@mail.ru>
2023-02-28 22:42:42 +0300

    Task39,40,42,44

---
+++ Task40/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Задача 40. Напишите программу, 
+// 1. которая принимает на вход три числа и
+//  2.принимает, может ли существовать треугольник с сторонами такой длины.
+//  каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Введите 3 числа");
+Console.Write("number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 < 1 || number2 < 1 || number3 < 1) Console.WriteLine("Введено некорректное число");
+
+bool Triangle(int num1, int num2, int num3)
+{
+    return num1 > num2 + num3 || num2 > num1 + num3 || num3 > num1 + num2;
+
+
+}
+
+bool result = Triangle(number1,number2,number3);
+if (result) Console.WriteLine("Такого треугольника не существует");
+else Console.WriteLine("Такой треугольник существует");
---
+++ Task42/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 42. Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101            //45/2=22(1)/2 = 11(0)/2 = 5(1)/2 = 2(1)/2=1(0)
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DecToBin(int num)
+{
+    int count = 1;
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 2 * count;
+        num = num / 2;
+        count *= 10;
+    }
+    return result;
+}
+int decToBin = DecToBin(number);
+Console.WriteLine($"{number} -> {decToBin}");
+
+
+// string DecToBin(int num)
+// {
+//     string newNum = string.Empty;
+//     while (num != 0)
+//     {
+//         newNum = num % 2 + newNum;
+//         num = num / 2;
+//     }
+//     return newNum;
+// }
+// string decToBin = DecToBin(number);
+// Console.WriteLine(decToBin);

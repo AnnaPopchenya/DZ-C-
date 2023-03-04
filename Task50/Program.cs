9091c68febc2accb4a8e31b9fb3f5c9bec7f6e61
Anna <popchenya995@mail.ru>
2023-03-04 22:10:41 +0300

    Added Task47,50,52

---
+++ Task50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];    //0,  1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  //columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool FindMatrixElement(int[,] matrix, int num1, int num2)
+{
+    if (matrix.GetLength(0) < num1 || matrix.GetLength(1) < num2) return true;
+    return false;
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+Console.WriteLine($"Введите позиции элемента двумерного массива ");
+Console.Write("m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+bool existNum = FindMatrixElement(array2d, m, n);
+Console.WriteLine(existNum ? "такого элемента нет" :
+$"значение элемента {m} строки и {n} столбца равно {array2d[m - 1, n - 1]}");
---
+++ Task52/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];    //0,  1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  //columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double AvarageSumElemsColumn(int[,] matrix)
+{
+    double avarage = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            avarage = (avarage + matrix[i, j]);
+        }
+        avarage = Math.Round((avarage / matrix.GetLength(0)), 1, MidpointRounding.ToZero);
+        Console.Write(avarage + "; ");
+    }
+    return avarage;
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(array2d);
+double avarageSumElemsColumn = AvarageSumElemsColumn(array2d);

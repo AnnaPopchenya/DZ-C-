a201350869bf8d09dac447df6259905c937d128f
Anna <popchenya995@mail.ru>
2023-03-09 23:01:05 +0300

    Task54,56,58

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -5,3 +5,56 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];    //0,  1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  //columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultipluMatrix(int[,] matrix1, int[,] matrix2, int[,] matrix3)
+{
+    int[,] matrix3 = new int[i, k];
+    for (int i = 0; i < matrix3.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                sum += Martrix1[i, k] * matrix2[k, j];
+            }
+            matrix3[i, j] = sum;
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+int[,] array2d2 = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(array2d2);
+MultiplyMatrix(Matrix1, Matrix2, Matrix3);
+Console.WriteLine($"Произведение первой и второй матриц:");
+PrintMatrix(Matrix3);
